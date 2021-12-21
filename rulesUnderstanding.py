11c5177916e79980149cea341b9b71e236e1e7b1
ThanhBinhTran <thanhbinh.hcmut@gmail.com>
2021-12-21 16:23:36 +0700

    clean up

--- rulesUnderstanding.py
+++ rulesUnderstanding.py
@@ -3,17 +3,25 @@ import xml.etree.ElementTree as ET
 import numpy as np
 
 rules_file = "rules.xml"
-
+param_file = "parameter.xml"
 ''' get name rule '''
 def get_rule_name(rule):
     return rule.attrib.get("name")
 
-''' get elements value '''
-def get_elements(rule, element_name):
-    element_tag = rule.find(element_name)
-    rule_element = element_tag.attrib.get(element_name)
+''' get elements value by tag'''
+def get_tag_elements(rule, element_name):
+    element = rule.find(element_name)
+    rule_element = element.attrib.get(element_name)
     #print("__________name: {0} data: {1}".format(element_name, rule_element) )
     return rule_element
+
+''' get parameter value '''
+def get_parameter_value(root, element_name):
+    for element in root:
+        print ("___________________", element.tag, element.attrib)
+        if element.attrib.get("name") == element_name:
+            return element.attrib.get("value")
+    return None
 
 ''' 
 apply rules in specific situation, return indexes of matching rules
@@ -63,31 +71,44 @@ def print_rules(rule_names, rule_conditions, rule_priorities,
     print ("_rule_priorities: ", rule_priorities)
     print ("_rule_confidences: ", rule_confidences)
 
-def main(rules_file):
+def main(rules_file, param_file):
     print(__file__ + " start!!")
+
+    ''' parsing rules '''
     tree = ET.parse(rules_file)
     root = tree.getroot()
-    print ("0__________", root.tag,"_" , root.attrib)
     rule_names = []
     rule_conditions = []
     rule_priorities = []
     rule_confidences = []
     rule_actions = []
     rule_names = np.array([get_rule_name(rule) for rule in root])
-    rule_conditions = np.array([get_elements(rule, "condition") for rule in root])
-    rule_priorities = np.array([get_elements(rule, "priority") for rule in root])
-    rule_confidences = np.array([get_elements(rule, "confidence") for rule in root])
-    rule_actions = np.array([get_elements(rule, "action") for rule in root])
+    rule_conditions = np.array([get_tag_elements(rule, "condition") for rule in root])
+    rule_priorities = np.array([get_tag_elements(rule, "priority") for rule in root])
+    rule_confidences = np.array([get_tag_elements(rule, "confidence") for rule in root])
+    rule_actions = np.array([get_tag_elements(rule, "action") for rule in root])
     rule_priorities = rule_priorities.astype(float)
+    rule_confidences = rule_confidences.astype(float)
 
     print_rules(rule_names, rule_conditions, rule_priorities, 
                 rule_confidences, rule_actions)
+    
+    ''' getting parameter '''
+    param_tree = ET.parse(param_file)
+    param_root = param_tree.getroot()
+
+
+    ''' applying for specific obstacle '''
+    OPENCV_obstacle = get_parameter_value(param_root, "OBSTACLE_FIXED")
+    LiDAR_distance = 30
 
     # get all rules matching specific situation
-    rule_condition_result = apply_specific_situation(obstacle = 1, distance = 30, rule_conditions = rule_conditions)
-    rule_idx = choose_rule(rule_condition_result, rule_priorities)
-    print_chosen_rule(rule_idx, rule_names, rule_conditions, rule_priorities, 
-                rule_confidences, rule_actions)
+    if OPENCV_obstacle is not None:
+        rule_condition_result = apply_specific_situation(obstacle = OPENCV_obstacle, distance = LiDAR_distance, 
+                    rule_conditions = rule_conditions)
+        rule_idx = choose_rule(rule_condition_result, rule_priorities)
+        print_chosen_rule(rule_idx, rule_names, rule_conditions, rule_priorities, 
+                    rule_confidences, rule_actions)
     
 if __name__ == '__main__':
-    main(rules_file)
+    main(rules_file, param_file)
